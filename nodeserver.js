adb4cf854370be94803e33a506d4728ed2c93f88
TechMan <telee@telees-mbp-15.lan>
2018-12-18 16:24:43 +0800

    Initial

---
+++ nodeserver.js
@@ -0,0 +1,21 @@
+http  = require("http")
+cfenv = require("cfenv")
+
+// get CF environmental variables
+appEnv   = cfenv.getAppEnv()
+instance = appEnv.app.instance_index || 0
+
+// create a server 
+server = http.createServer(onRequest)
+
+// start the server 
+server.listen(appEnv.port, function() {
+    console.log("server starting on " + appEnv.url)
+})
+
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"})
+
+  response.end("Hello GREENIE World - v1 - running on instance [" + instance + "] <BR>\n")
+  response.end("\n")
+}
